0a9383fa919339326f61a7c1c86d1c67939ee909
miscojones3434 <miscojones424@gmail.com>
2026-07-30 19:44:22 +0200

    Eliminar import no utilizado de pantalla PT34

--- lib/pt34/pages/pt34_home_page.dart
+++ lib/pt34/pages/pt34_home_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
-import 'package:sliver_tools/sliver_tools.dart';
 import 'package:spotube/components/titlebar/titlebar.dart';
 import 'package:spotube/pt34/core/pt34_brand.dart';
 
